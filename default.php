b7b38de20499108fc6c6e2420e240479b9626d46
Prashant1405 <prashantkumar.mishra@minjar.com>
2015-02-05 18:42:03 +0530

    first commit

---
+++ default.php
@@ -0,0 +1,7 @@
+<form enctype="multipart/form-data" method="post"action ="file_uploader.php">
+Choose your file here:
+<input name="file1" type="file" /><br /><br />
+<input type="submit" value="Upload It" />
+<a href= delete.php>delete all uploads</a>
+<br>
+<br>
---
+++ index.php
@@ -0,0 +1,10 @@
+
+
+<form enctype="multipart/form-data" method="post"action ="file_upload_script.php">
+Choose your file here:
+<input name="file1" type="file" /><br /><br />
+<input type="submit" value="Upload It" />
+
+<a href= default.php>For 5000 times upload</a>
+<br>
+<br>
